1526329ba5756734742b4fbb25b62f9bfecf83a8
Samuel Sjoberg <samuel.sjoberg@extenda.se>
2019-01-29 13:10:03 +0100

    Increment in-memory ID

---
+++ index.js
@@ -0,0 +1,22 @@
+const fastify = require('fastify')({
+  logger: false
+})
+
+let workstationId = 0;
+
+fastify.get('/', async(request, response) => {
+  workstationId += 1
+  return workstationId
+})
+
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000)
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+start()
